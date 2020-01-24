65d63e57bc16b4c41d138da9eca431ace0c117de
Jérôme BUISINE <contact@jeromebuisine.fr>
2020-01-24 21:21:51 +0100

    Project initialization

---
+++ README.md
@@ -0,0 +1,24 @@
+# Noise detection with CNN
+
+## Requirements
+
+```bash
+git clone --recursive https://github.com/prise-3d/Sampling-denoising.git
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+Generate necessary data from `.rawls` files
+```
+bash generate_data.sh scenes_folder output_folder 100
+```
+
+## How to use ?
+
+Implemented later...
+
+## License
+
+[MIT](LICENSE)
---
+++ generate_data.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied"
+    echo "Need data folder"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No argument supplied"
+    echo "Need output folder"
+    exit 1
+fi
+
+if [ -z "$3" ]
+  then
+    echo "No argument supplied"
+    echo "Need nomber of images expected (1 samples images)"
+    exit 1
+fi
+
+prefix="p3d_"
+build_folder="rawls-tools/build"
+
+data_folder=$1
+output_folder=$2
+nb_elements=$3
+
+for folder_path in $(ls -d ${data_folder}*/)
+do
+
+  IFS='/' read -ra ADDR <<< "${folder_path}"
+  folder=${ADDR[-1]}
+
+  # get output expected path
+  output_scene_path=$output_folder/$folder
+  output_scene_path_fixed=${output_scene_path//\/\//\/}
+
+  if [ ! -d "$output_scene_path_fixed" ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    
+    mkdir -p $output_scene_path_fixed
+
+    suffix=
+
+    for i in $(seq 1 ${nb_elements}); do
+
+        suffix=$i
+        size=${#suffix} 
+    
+        while [ $size -le 5 ]
+        do
+            suffix="0${suffix}"
+            size=${#suffix} 
+        done
+
+
+        ./${build_folder}/main/rawls_merge_mean --folder ${folder_path} --samples 1 --outfile $output_scene_path_fixed/${folder}_${suffix}.png --random 1
+    done
+      
+  fi
+done
